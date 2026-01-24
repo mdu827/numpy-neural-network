Update Tensor, Module, add Tanh activation, fix backprop

main.py
@@ -1,15 +1,46 @@
 from numpy_neural_network.tensor import Tensor
-from numpy_neural_network.nn.Module import Linear
+from numpy_neural_network.nn.Module import *
+from numpy_neural_network.optimizer.sgd import *
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
 
-x = Tensor([[1., 2., 3.]], requires_grad=True)  # (1,3)
+data = load_diabetes()
+X_np, Y_np = data.data, data.target.reshape(-1, 1)
+X_train, X_test, Y_train, Y_test = train_test_split(X_np, Y_np, test_size=0.2, random_state=42)
+X_train = Tensor(X_train, requires_grad=False)
+Y_train = Tensor(Y_train, requires_grad=False)
+X_test = Tensor(X_test, requires_grad=False)
+Y_test = Tensor(Y_test, requires_grad=False)
+model = Sequential(
+    Linear(X_train.data.shape[1], 16),
+    Tanh(),
+    Linear(16,1)
+)
 
-lin = Linear(3, 2)
+y_pred = model(X_test)
+mse = ((y_pred - Y_test) ** 2).mean().data.item()
+print("Test MSE before training:", mse)
 
-y = lin(x)
-loss = y.sum()
-loss.backward()
+optimizer = SGD(model.parameters(), lr=0.001)
+epochs = 10000
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    
+    # Прямой проход
+    y_pred = model(X_train)
+    
+    # MSE Loss
+    loss = ((y_pred - Y_train) ** 2).mean()
+    
+    # Обратный проход
+    loss.backward()
+    
+    # Шаг оптимизатора
+    optimizer.step()
+    
+    if epoch % 1000 == 0:
+        print(f"Epoch {epoch}, loss: {loss.data}")
 
-print("y:", y.data)
-print("x.grad:", x.grad)
-print("W.grad:", lin.W.grad)
-print("b.grad:", lin.b.grad)
+y_pred = model(X_test)
+mse = ((y_pred - Y_test) ** 2).mean().data.item()
+print("Test MSE after training:", mse)

nn/Module.py
@@ -1,27 +1,99 @@
 from numpy_neural_network.tensor import Tensor
 import numpy as np
 
-class Linear:
-    def __init__(self, in_features, out_features):
-        self.W = Tensor(np.random.randn(in_features, out_features), requires_grad=True)
-        self.b = Tensor(np.zeros(out_features), requires_grad=True)
+class Module:
+    def parameters(self):
+        params = []
 
+        for value in self.__dict__.values():
+            if isinstance(value, Tensor):
+                if value.requires_grad:
+                    params.append(value)
+
+            elif isinstance(value, Module):
+                params.extend(value.parameters())
+
+            elif isinstance(value, (list, tuple)):
+                for item in value:
+                    if isinstance(item, Module):
+                        params.extend(item.parameters())
+
+        return params
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.zero_grad()
+
+class ReLU(Module):
     def __call__(self, x):
-        return x @ self.W + self.b
+        out = None  # создаём переменную для замыкания
 
-class Linear:
-    def __init__(self, in_features, out_features,):
-        # weights
+        def _backward():
+            if x.requires_grad:
+                # используем out.grad, которое будет установлено в Tensor
+                grad = (x.data > 0) * out.grad
+                Tensor._add_grad(x, grad)
+
+        out = x._create_child(
+            np.maximum(0, x.data),
+            (x,),
+            "relu",
+            _backward
+        )
+        return out
+
+class Tanh(Module):
+    def __call__(self, x):
+        out = None  # placeholder для замыкания
+
+        def _backward():
+            if x.requires_grad:
+                grad = (1 - np.tanh(x.data) ** 2) * out.grad
+                Tensor._add_grad(x, grad)
+
+        out = x._create_child(
+            np.tanh(x.data),  # данные после tanh
+            (x,),             # prev
+            "tanh",           # имя операции
+            _backward         # backward функция
+        )
+        return out
+
+
+class Linear(Module):
+    def __init__(self, in_features, out_features):
         self.W = Tensor(
-            np.random.randn(in_features, out_features),
+            np.random.randn(in_features, out_features) * 0.1,
             requires_grad=True
         )
-
-        # bias
         self.b = Tensor(
             np.zeros(out_features),
             requires_grad=True
         )
 
-    def __call__(self, x):
+    def forward(self, x):
         return x @ self.W + self.b
+
+    def __call__(self, x):
+        return self.forward(x)
+
+
+class MLP(Module):
+    def __init__(self):
+        self.l1 = Linear(2, 4)
+        self.relu = ReLU()
+        self.l2 = Linear(4, 1)
+
+    def __call__(self, x):
+        x = self.l1(x)
+        x = self.relu(x)
+        x = self.l2(x)
+        return x
+class Sequential(Module):
+    def __init__(self, *layers):
+        self.layers = layers
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x